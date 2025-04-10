ccb60a465dc60639a15122d8d996d1dfebf6531a
Lukasz Szpakowski <luckboy@vp.pl>
2025-04-10 15:56:35 +0200

    Added methods to backend and frontend.

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,58 @@ pub mod opencl;
 
 pub trait Backend
 {
+    fn alloc(&self, n: usize, m: usize) -> Result<BackendArray>;
+
+    fn alloc_with_elems(&self, elems: &[f32], n: usize, m: usize) -> Result<BackendArray>;
+    
+    fn load(&self, a: &BackendArray, n: usize, m: usize) -> Result<Vec<f32>>;
+
+    fn add_a_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn add_at_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+    
+    fn add_a_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn add_at_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn sub_a_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn sub_at_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+    
+    fn sub_a_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn sub_at_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;    
+    
+    fn mul_a_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>;
+
+    fn mul_at_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>;
+
+    fn mul_a_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>;
+
+    fn mul_at_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>;
+
+    fn mul_a_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn mul_at_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+    
+    fn mul_a_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+    
+    fn mul_at_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn div_a_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+
+    fn div_at_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+    
+    fn div_a_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
+    
+    fn div_at_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>;
 }
 
 pub enum Error
 {
+    OpSize(usize, usize, usize, usize),
+    MulSize(usize, usize, usize, usize, usize, usize),
+    ResTransposition,
     Mutex,
     #[cfg(feature = "opencl")]
     OpenCl(opencl::ClError),
@@ -26,4 +74,138 @@ pub enum Error
 
 pub type Result<T> = result::Result<T, Error>;
 
+pub enum BackendArray
+{
+    #[cfg(feature = "opencl")]
+    OpenCl(opencl::ClBackendArray),
+}
+
 static mut DEFAULT_BACKEND: Mutex<Option<Arc<dyn Backend>>> = Mutex::new(None);
+
+#[derive(Clone)]
+pub struct Matrix
+{
+    row_count: usize,
+    col_count: usize,
+    is_transposed: bool,
+    array: Arc<BackendArray>,
+}
+
+impl Matrix
+{
+    pub fn transpose(&self) -> Matrix
+    {
+        Matrix {
+            row_count: self.col_count,
+            col_count: self.row_count,
+            is_transposed: !self.is_transposed,
+            array: self.array.clone(),
+        }
+    }
+}
+
+pub struct Frontend
+{
+    backend: Arc<dyn Backend>,
+}
+
+impl Frontend
+{
+    pub fn add(&self, a: &Matrix, b: &Matrix, c: &Matrix) -> Result<()>
+    {
+        if a.row_count == b.row_count && a.col_count == b.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, b.row_count, b.col_count)); 
+        }
+        if a.row_count == c.row_count && a.col_count == c.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, c.row_count, c.col_count)); 
+        }
+        if c.is_transposed {
+            return Err(Error::ResTransposition);
+        }
+        match (a.is_transposed, a.is_transposed) {
+            (false, false) => self.backend.add_a_b(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, false) => self.backend.add_at_b(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (false, true) => self.backend.add_a_bt(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, true) => self.backend.add_at_bt(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+        }
+    }
+
+    pub fn sub(&self, a: &Matrix, b: &Matrix, c: &Matrix) -> Result<()>
+    {
+        if a.row_count == b.row_count && a.col_count == b.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, b.row_count, b.col_count)); 
+        }
+        if a.row_count == c.row_count && a.col_count == c.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, c.row_count, c.col_count)); 
+        }
+        if c.is_transposed {
+            return Err(Error::ResTransposition);
+        }
+        match (a.is_transposed, a.is_transposed) {
+            (false, false) => self.backend.sub_a_b(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, false) => self.backend.sub_at_b(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (false, true) => self.backend.sub_a_bt(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, true) => self.backend.sub_at_bt(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+        }
+    }
+
+    pub fn mul(&self, a: &Matrix, b: &Matrix, c: &Matrix) -> Result<()>
+    {
+        if a.row_count == c.row_count {
+            return Err(Error::MulSize(a.row_count, a.col_count, b.row_count, b.col_count, c.row_count, c.col_count)); 
+        }
+        if b.col_count == c.col_count {
+            return Err(Error::MulSize(a.row_count, a.col_count, b.row_count, b.col_count, c.row_count, c.col_count)); 
+        }
+        if a.col_count == b.row_count {
+            return Err(Error::MulSize(a.row_count, a.col_count, b.row_count, b.col_count, c.row_count, c.col_count)); 
+        }
+        if c.is_transposed {
+            return Err(Error::ResTransposition);
+        }
+        match (a.is_transposed, a.is_transposed) {
+            (false, false) => self.backend.mul_a_b(&*a.array, &*b.array, &*c.array, a.row_count, b.col_count, a.col_count),
+            (true, false) => self.backend.mul_at_b(&*a.array, &*b.array, &*c.array, a.row_count, b.col_count, a.col_count),
+            (false, true) => self.backend.mul_a_bt(&*a.array, &*b.array, &*c.array, a.row_count, b.col_count, a.col_count),
+            (true, true) => self.backend.mul_at_bt(&*a.array, &*b.array, &*c.array, a.row_count, b.col_count, a.col_count),
+        }
+    }
+
+    pub fn mul_elems(&self, a: &Matrix, b: &Matrix, c: &Matrix) -> Result<()>
+    {
+        if a.row_count == b.row_count && a.col_count == b.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, b.row_count, b.col_count)); 
+        }
+        if a.row_count == c.row_count && a.col_count == c.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, c.row_count, c.col_count)); 
+        }
+        if c.is_transposed {
+            return Err(Error::ResTransposition);
+        }
+        match (a.is_transposed, a.is_transposed) {
+            (false, false) => self.backend.mul_a_b_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, false) => self.backend.mul_at_b_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (false, true) => self.backend.mul_a_bt_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, true) => self.backend.mul_at_bt_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+        }
+    }
+
+    pub fn div_elems(&self, a: &Matrix, b: &Matrix, c: &Matrix) -> Result<()>
+    {
+        if a.row_count == b.row_count && a.col_count == b.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, b.row_count, b.col_count)); 
+        }
+        if a.row_count == c.row_count && a.col_count == c.col_count {
+            return Err(Error::OpSize(a.row_count, a.col_count, c.row_count, c.col_count)); 
+        }
+        if c.is_transposed {
+            return Err(Error::ResTransposition);
+        }
+        match (a.is_transposed, a.is_transposed) {
+            (false, false) => self.backend.div_a_b_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, false) => self.backend.div_at_b_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (false, true) => self.backend.div_a_bt_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+            (true, true) => self.backend.div_at_bt_for_elems(&*a.array, &*b.array, &*c.array, a.row_count, a.col_count),
+        }
+    }
+}
--- src/opencl.rs
+++ src/opencl.rs
@@ -6,6 +6,7 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
 use crate::Backend;
+use crate::BackendArray;
 use crate::Result;
 
 pub use opencl3::context::Context;
@@ -13,6 +14,10 @@ pub use opencl3::device::Device;
 pub use opencl3::error_codes::ClError;
 pub use opencl3::platform::Platform;
 pub use opencl3::platform::get_platforms;
+
+pub struct ClBackendArray
+{
+}
 
 pub struct ClBackend
 {
@@ -30,4 +35,72 @@ impl ClBackend
 
 impl Backend for ClBackend
 {
+    fn alloc(&self, n: usize, m: usize) -> Result<BackendArray>
+    { Ok(BackendArray::OpenCl(ClBackendArray {})) }
+
+    fn alloc_with_elems(&self, elems: &[f32], n: usize, m: usize) -> Result<BackendArray>
+    { Ok(BackendArray::OpenCl(ClBackendArray {})) }
+    
+    fn load(&self, a: &BackendArray, n: usize, m: usize) -> Result<Vec<f32>>
+    { Ok(Vec::new()) }
+    
+    fn add_a_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn add_at_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+    
+    fn add_a_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn add_at_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn sub_a_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn sub_at_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+    
+    fn sub_a_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn sub_at_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>    
+    { Ok(()) }
+    
+    fn mul_a_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>
+    { Ok(()) }
+
+    fn mul_at_b(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>
+    { Ok(()) }
+
+    fn mul_a_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>
+    { Ok(()) }
+
+    fn mul_at_bt(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize, l: usize) -> Result<()>
+    { Ok(()) }
+
+    fn mul_a_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn mul_at_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+    
+    fn mul_a_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+    
+    fn mul_at_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn div_a_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+
+    fn div_at_b_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+    
+    fn div_a_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
+    
+    fn div_at_bt_for_elems(&self, a: &BackendArray, b: &BackendArray, c: &BackendArray, n: usize, m: usize) -> Result<()>
+    { Ok(()) }
 }
