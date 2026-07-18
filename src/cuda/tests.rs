ed8c0de96b5cf55a82aa9759d0352588776d5947
Lukasz Szpakowski <luckboy@vp.pl>
2026-07-18 06:11:04 +0200

    Fixed test for CUDA backend.

--- src/cuda/tests.rs
+++ src/cuda/tests.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2025 Łukasz Szpakowski
+// Copyright (c) 2025-2026 Łukasz Szpakowski
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -1912,7 +1912,7 @@ fn test_cuda_backend_atan2_a_b_for_scalar_calculates_atan2_for_backend_array_and
 {
     match CudaBackend::new() {
         Ok(backend) => {
-            let a = fixture_a_for_log(2, 3);
+            let a = fixture_a_for_common_math_fun(2, 3);
             match backend_atan2_a_b_for_scalar(&backend, a.as_slice(), 10.5, 2, 3) {
                 Ok(c) => {
                     let expected_c = expected_atan2_a_b_for_scalar(a.as_slice(), 10.5, 2, 3);
